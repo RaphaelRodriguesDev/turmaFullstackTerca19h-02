86f3e6cad2615aff96aed688d10ff1268fa01f37
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-05-27 20:55:14 -0300

    Aula listas

---
+++ aula13/for-in.js
@@ -0,0 +1,4 @@
+const numeros = [1, 2, 3, 4, 5];
+for (let indice in numeros) {
+ console.log(numeros[indice]);
+}
---
+++ aula13/pop-push-shift-unshift.js
@@ -0,0 +1,12 @@
+const nomes = [];
+
+nomes.push("José");
+nomes.push("Ricardo");
+nomes.push("Patrícia");
+nomes.pop()
+nomes.unshift("Carlos");
+nomes.unshift("Roberta");
+nomes.unshift("Joana");
+nomes.shift();
+
+console.log(nomes);
