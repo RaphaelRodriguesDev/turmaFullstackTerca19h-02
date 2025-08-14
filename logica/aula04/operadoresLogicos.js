8f38e4b00fd79f0fdcaff28f1d66e088cde3487c
RaphaelRodriguesDev <professor.dev.raphael@gmail.com>
2025-08-14 15:58:48 -0300

    Headings e links

--- logica/aula04/operadoresLogicos.js
+++ logica/aula04/operadoresLogicos.js
@@ -3,6 +3,6 @@ let n2 = 20;
 
 // let verificador = n1 < n2 && n1 == 30; // AND | &&
 // let verificador = n1 < n2 || n1 == 20; // OU | ||
-let verificador = !(n1 == 30); // AND | &&
+let verificador = !(n1 == 30); // NOT | NÃO
 
 console.log(verificador);
